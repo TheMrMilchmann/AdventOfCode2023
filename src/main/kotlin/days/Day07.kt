9eb364e45cd8553e54059b1c8c4d46cd72cb85a0
Leon Linhart <themrmilchmann@gmail.com>
2023-12-07 12:15:22 +0100

    feat(07): part 1

---
+++ src/main/kotlin/days/Day07.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2023 Leon Linhart
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package days
+
+import utils.*
+
+fun main() {
+    data class Hand(val type: HandType, val cards: String) : Comparable<Hand> {
+
+        override fun compareTo(other: Hand): Int {
+            val symbols = listOf('A', 'K', 'Q', 'J', 'T')
+            val cmp: Comparator<Hand> = compareBy(Hand::type)
+                .then { alpha, beta ->
+                    for (i in 0..<5) {
+                        val a = alpha.cards[i]
+                        val aI = symbols.indexOf(a)
+
+                        val b = beta.cards[i]
+                        val bI = symbols.indexOf(b)
+
+                        when {
+                            aI >= 0 && (bI < 0 || bI > aI) -> return@then 1
+                            aI >= 0 && (bI < aI) -> return@then -1
+                            aI < 0 && bI >= 0 -> return@then -1
+                            else -> {
+                                val x = a.compareTo(b)
+                                if (x != 0) return@then x
+                            }
+                        }
+                    }
+
+                    0
+                }
+
+            return cmp.compare(this, other)
+        }
+
+        override fun toString(): String = cards
+
+    }
+
+    val entries = readInput().map { it.split(' ') }
+        .map { (cards, bid) ->
+            val groups = cards.groupBy { it }
+
+            val hand = Hand(
+                type = when {
+                    groups.size == 1 -> HandType.FIVE
+                    groups.any { (_, values) -> values.size == 4 } -> HandType.FOUR
+                    groups.any { (_, values) -> values.size == 3 } -> when {
+                        groups.any { (_, values) -> values.size == 2 } -> HandType.FULL_HOUSE
+                        else -> HandType.THREE
+                    }
+                    groups.count { (_, values) -> values.size == 2 } == 2 -> HandType.TWO_PAIR
+                    groups.any { (_, values) -> values.size == 2 } -> HandType.ONE_PAIR
+                    else -> HandType.HIGH_CARD
+                },
+                cards = cards
+            )
+
+            hand to bid.toLong()
+        }
+
+    println("Part 1: ${entries.sortedBy(Pair<Hand, Long>::first).also { println(it) }.mapIndexed { index, (_, bid) -> (index + 1) * bid }.sum()}")
+}
+
+private enum class HandType {
+    HIGH_CARD,
+    ONE_PAIR,
+    TWO_PAIR,
+    THREE,
+    FULL_HOUSE,
+    FOUR,
+    FIVE
+}
