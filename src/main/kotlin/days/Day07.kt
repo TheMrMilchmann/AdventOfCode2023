8af2fa2ecdf510349fe870172a70452c83349c30
Leon Linhart <themrmilchmann@gmail.com>
2023-12-07 12:44:29 +0100

    feat(07): part 2

--- src/main/kotlin/days/Day07.kt
+++ src/main/kotlin/days/Day07.kt
@@ -24,63 +24,138 @@ package days
 import utils.*
 
 fun main() {
-    data class Hand(val type: HandType, val cards: String) : Comparable<Hand> {
-
-        override fun compareTo(other: Hand): Int {
-            val symbols = listOf('A', 'K', 'Q', 'J', 'T')
-            val cmp: Comparator<Hand> = compareBy(Hand::type)
-                .then { alpha, beta ->
-                    for (i in 0..<5) {
-                        val a = alpha.cards[i]
-                        val aI = symbols.indexOf(a)
-
-                        val b = beta.cards[i]
-                        val bI = symbols.indexOf(b)
-
-                        when {
-                            aI >= 0 && (bI < 0 || bI > aI) -> return@then 1
-                            aI >= 0 && (bI < aI) -> return@then -1
-                            aI < 0 && bI >= 0 -> return@then -1
-                            else -> {
-                                val x = a.compareTo(b)
-                                if (x != 0) return@then x
-                            }
-                        }
-                    }
-
-                    0
-                }
-
-            return cmp.compare(this, other)
-        }
-
+    data class Hand(val type: HandType, val cards: String) {
         override fun toString(): String = cards
-
     }
 
-    val entries = readInput().map { it.split(' ') }
-        .map { (cards, bid) ->
-            val groups = cards.groupBy { it }
+    fun part1(): Long {
+        val entries = readInput().map { it.split(' ') }
+            .map { (cards, bid) ->
+                val groups = cards.groupBy { it }
 
-            val hand = Hand(
-                type = when {
-                    groups.size == 1 -> HandType.FIVE
-                    groups.any { (_, values) -> values.size == 4 } -> HandType.FOUR
-                    groups.any { (_, values) -> values.size == 3 } -> when {
-                        groups.any { (_, values) -> values.size == 2 } -> HandType.FULL_HOUSE
-                        else -> HandType.THREE
+                val hand = Hand(
+                    type = when {
+                        groups.size == 1 -> HandType.FIVE
+                        groups.any { (_, values) -> values.size == 4 } -> HandType.FOUR
+                        groups.any { (_, values) -> values.size == 3 } -> when {
+                            groups.any { (_, values) -> values.size == 2 } -> HandType.FULL_HOUSE
+                            else -> HandType.THREE
+                        }
+                        groups.count { (_, values) -> values.size == 2 } == 2 -> HandType.TWO_PAIR
+                        groups.any { (_, values) -> values.size == 2 } -> HandType.ONE_PAIR
+                        else -> HandType.HIGH_CARD
+                    },
+                    cards = cards
+                )
+
+                hand to bid.toLong()
+            }
+
+        val symbols = listOf('A', 'K', 'Q', 'J', 'T')
+        val cmp: Comparator<Hand> = compareBy(Hand::type)
+            .then { alpha, beta ->
+                for (i in 0..<5) {
+                    val a = alpha.cards[i]
+                    val aI = symbols.indexOf(a)
+
+                    val b = beta.cards[i]
+                    val bI = symbols.indexOf(b)
+
+                    when {
+                        aI >= 0 && (bI < 0 || bI > aI) -> return@then 1
+                        aI >= 0 && (bI < aI) -> return@then -1
+                        aI < 0 && bI >= 0 -> return@then -1
+                        else -> {
+                            val x = a.compareTo(b)
+                            if (x != 0) return@then x
+                        }
                     }
-                    groups.count { (_, values) -> values.size == 2 } == 2 -> HandType.TWO_PAIR
-                    groups.any { (_, values) -> values.size == 2 } -> HandType.ONE_PAIR
-                    else -> HandType.HIGH_CARD
-                },
-                cards = cards
-            )
+                }
 
-            hand to bid.toLong()
-        }
+                0
+            }
 
-    println("Part 1: ${entries.sortedBy(Pair<Hand, Long>::first).also { println(it) }.mapIndexed { index, (_, bid) -> (index + 1) * bid }.sum()}")
+        return entries.sortedWith { (a, _), (b, _) -> cmp.compare(a, b) }.mapIndexed { index, (_, bid) -> (index + 1) * bid }.sum()
+    }
+
+    fun part2(): Long {
+        val entries = readInput().map { it.split(' ') }
+            .map { (cards, bid) ->
+                val groups = cards.filter { it != 'J' }.groupBy { it }
+                val jokers = cards.count { it == 'J' }
+
+                val hand = Hand(
+                    type = when {
+                        jokers == 5 || groups.size == 1 -> HandType.FIVE
+                        groups.any { (_, values) -> values.size == 4 } -> when (jokers) {
+                            1 -> HandType.FIVE
+                            else -> HandType.FOUR
+                        }
+                        groups.any { (_, values) -> values.size == 3 } -> when (jokers) {
+                            2 -> HandType.FIVE
+                            1 -> HandType.FOUR
+                            else -> when {
+                                groups.any { (_, values) -> values.size == 2 } -> HandType.FULL_HOUSE
+                                else -> HandType.THREE
+                            }
+                        }
+                        groups.count { (_, values) -> values.size == 2 } == 2 -> when (jokers) {
+                            3 -> HandType.FIVE
+                            2 -> HandType.FOUR
+                            1 -> HandType.FULL_HOUSE
+                            else -> HandType.TWO_PAIR
+                        }
+                        groups.any { (_, values) -> values.size == 2 } -> when (jokers) {
+                            3 -> HandType.FIVE
+                            2 -> HandType.FOUR
+                            1 -> HandType.THREE
+                            else -> HandType.ONE_PAIR
+                        }
+                        else -> when (jokers) {
+                            4 -> HandType.FIVE
+                            3 -> HandType.FOUR
+                            2 -> HandType.THREE
+                            1 -> HandType.ONE_PAIR
+                            else -> HandType.HIGH_CARD
+                        }
+                    },
+                    cards = cards
+                )
+
+                hand to bid.toLong()
+            }
+
+        val symbols = listOf('A', 'K', 'Q', 'T')
+        val cmp: Comparator<Hand> = compareBy(Hand::type)
+            .then { alpha, beta ->
+                for (i in 0..<5) {
+                    val a = alpha.cards[i]
+                    val aI = symbols.indexOf(a)
+
+                    val b = beta.cards[i]
+                    val bI = symbols.indexOf(b)
+
+                    when {
+                        a == 'J' && b != 'J' -> return@then -1
+                        a != 'J' && b == 'J' -> return@then 1
+                        aI >= 0 && (bI < 0 || bI > aI) -> return@then 1
+                        aI >= 0 && (bI < aI) -> return@then -1
+                        aI < 0 && bI >= 0 -> return@then -1
+                        else -> {
+                            val x = a.compareTo(b)
+                            if (x != 0) return@then x
+                        }
+                    }
+                }
+
+                0
+            }
+
+        return entries.sortedWith { (a, _), (b, _) -> cmp.compare(a, b) }.mapIndexed { index, (_, bid) -> (index + 1) * bid }.sum()
+    }
+
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
 }
 
 private enum class HandType {
